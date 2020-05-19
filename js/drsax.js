8012a2cd0b866cb947153cbced0ba369a6f8c90a
drhon9 <drhon9@naver.com>
2020-05-19 13:18:52 +0900

    rivise rvb file

--- js/drsax.js
+++ js/drsax.js
@@ -883,7 +883,7 @@ const DrSax_revision = "worked_2020_05_18_r1";
         var reverbfile;
         var revebRequest = new XMLHttpRequest();
     
-        revebRequest.open('GET', 'https://dhon9.github.io/drsaxCore/import/rvb.wav', true);
+        revebRequest.open('GET', '../import/rvb.wav', true);
         revebRequest.responseType = 'arraybuffer';
         revebRequest.send();
 
